1165b1da839cf57e6dd1bad13d4248f731660cdc
baifan <baifanxxx@gmail.com>
2020-11-13 23:06:19 +0800

    hw4

---
+++ assignment4/train_TD3_noCDQ.py
@@ -0,0 +1,291 @@
+# The following scripts run the TD3 algorithm.
+
+alias = 'td3_noCDQ'
+import gym
+import utils
+
+import DDPG
+
+import copy
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# Implementation of Twin Delayed Deep Deterministic Policy Gradients (TD3)
+# Paper: https://arxiv.org/abs/1802.09477
+
+ENV_NAME = "Hopper-v3"
+seed = 15
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, action_dim, max_action):
+        super(Actor, self).__init__()
+
+        self.l1 = nn.Linear(state_dim, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, action_dim)
+
+        self.max_action = max_action
+
+    def forward(self, state):
+        a = F.relu(self.l1(state))
+        a = F.relu(self.l2(a))
+        return self.max_action * torch.tanh(self.l3(a))
+
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Critic, self).__init__()
+
+        # Q1 architecture
+        self.l1 = nn.Linear(state_dim + action_dim, 256)
+        self.l2 = nn.Linear(256, 256)
+        self.l3 = nn.Linear(256, 1)
+
+        # Q2 architecture
+        self.l4 = nn.Linear(state_dim + action_dim, 256)
+        self.l5 = nn.Linear(256, 256)
+        self.l6 = nn.Linear(256, 1)
+
+    def forward(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.l1(sa))
+        q1 = F.relu(self.l2(q1))
+        q1 = self.l3(q1)
+
+        q2 = F.relu(self.l4(sa))
+        q2 = F.relu(self.l5(q2))
+        q2 = self.l6(q2)
+        return q1, q2
+
+    def Q1(self, state, action):
+        sa = torch.cat([state, action], 1)
+
+        q1 = F.relu(self.l1(sa))
+        q1 = F.relu(self.l2(q1))
+        q1 = self.l3(q1)
+        return q1
+
+
+class TD3(object):
+    def __init__(
+            self,
+            state_dim,
+            action_dim,
+            max_action,
+            discount=0.99,
+            tau=0.005,
+            policy_noise=0.2,
+            noise_clip=0.5,
+            policy_freq=2
+    ):
+
+        self.actor = Actor(state_dim, action_dim, max_action).to(device)
+        self.actor_target = copy.deepcopy(self.actor)
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(), lr=3e-4)
+
+        self.critic = Critic(state_dim, action_dim).to(device)
+        self.critic_target = copy.deepcopy(self.critic)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=3e-4)
+
+        self.max_action = max_action
+        self.discount = discount
+        self.tau = tau
+        self.policy_noise = policy_noise
+        self.noise_clip = noise_clip
+        self.policy_freq = policy_freq
+
+        self.total_it = 0
+
+    def select_action(self, state):
+        state = torch.FloatTensor(state.reshape(1, -1)).to(device)
+        return self.actor(state).cpu().data.numpy().flatten()
+
+    def train(self, replay_buffer, batch_size=100):
+        self.total_it += 1
+
+        # Sample replay buffer
+        state, action, next_state, reward, not_done = replay_buffer.sample(batch_size)
+
+        with torch.no_grad():
+            # Select action according to policy and add clipped noise
+            noise = (
+            	torch.randn_like(action) * self.policy_noise
+            ).clamp(-self.noise_clip, self.noise_clip)
+
+            next_action = (
+            	self.actor_target(next_state) + noise
+            ).clamp(-self.max_action, self.max_action)
+
+
+            # Compute the target Q value
+            target_Q1, target_Q2 = self.critic_target(next_state, next_action)
+            #target_Q = torch.min(target_Q1, target_Q2)
+            target_Q = target_Q1
+            target_Q = reward + not_done * self.discount * target_Q
+
+        # Get current Q estimates
+        current_Q1, current_Q2 = self.critic(state, action)
+
+        # Compute critic loss
+        critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+
+        # Optimize the critic
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        # Delayed policy updates
+        if self.total_it % self.policy_freq == 0:
+
+            # Compute actor losse
+            actor_loss = -self.critic.Q1(state, self.actor(state)).mean()
+
+            # Optimize the actor
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+
+            # Update the frozen target models
+            for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+                target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+            for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+                target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
+
+    def save(self, filename):
+        torch.save(self.critic.state_dict(), filename + "_critic")
+        torch.save(self.critic_optimizer.state_dict(), filename + "_critic_optimizer")
+
+        torch.save(self.actor.state_dict(), filename + "_actor")
+        torch.save(self.actor_optimizer.state_dict(), filename + "_actor_optimizer")
+
+    def load(self, filename):
+        self.critic.load_state_dict(torch.load(filename + "_critic"))
+        self.critic_optimizer.load_state_dict(torch.load(filename + "_critic_optimizer"))
+        self.critic_target = copy.deepcopy(self.critic)
+
+        self.actor.load_state_dict(torch.load(filename + "_actor"))
+        self.actor_optimizer.load_state_dict(torch.load(filename + "_actor_optimizer"))
+        self.actor_target = copy.deepcopy(self.actor)
+
+
+def eval_policy(policy, eval_episodes=10):
+    eval_env = gym.make(ENV_NAME)
+
+    avg_reward = 0.
+    for _ in range(eval_episodes):
+        state, done = eval_env.reset(), False
+        while not done:
+            action = policy.select_action(np.array(state))
+            state, reward, done, _ = eval_env.step(action)
+            avg_reward += reward
+
+    avg_reward /= eval_episodes
+    # print("---------------------------------------")
+    # print(f"Evaluation over {eval_episodes} episodes: {avg_reward:.3f}")
+    # print("---------------------------------------")
+    return avg_reward
+
+
+env = gym.make(ENV_NAME)
+torch.manual_seed(seed)
+np.random.seed(seed)
+
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.shape[0]
+max_action = env.action_space.high[0]
+
+args_policy_noise = 0.2
+args_noise_clip = 0.5
+args_policy_freq = 2
+args_max_timesteps = 100000
+args_expl_noise = 0.1
+args_batch_size = 25
+args_eval_freq = 100
+args_start_timesteps = 0
+
+kwargs = {
+    "state_dim": state_dim,
+    "action_dim": action_dim,
+    "max_action": max_action,
+    "discount": 0.99,
+    "tau": 0.005
+}
+
+args_policy = 'TD3'
+
+if args_policy == "TD3":
+    # Target policy smoothing is scaled wrt the action scale
+    kwargs["policy_noise"] = args_policy_noise * max_action
+    kwargs["noise_clip"] = args_noise_clip * max_action
+    kwargs["policy_freq"] = args_policy_freq
+    policy = TD3(**kwargs)
+elif args_policy == "OurDDPG":
+    policy = OurDDPG.DDPG(**kwargs)
+elif args_policy == "DDPG":
+    policy = DDPG.DDPG(**kwargs)
+replay_buffer = utils.ReplayBuffer(state_dim, action_dim)
+
+# Evaluate untrained policy
+evaluations = [eval_policy(policy)]
+
+state, done = env.reset(), False
+episode_reward = 0
+episode_timesteps = 0
+episode_num = 0
+counter = 0
+msk_list = []
+temp_curve = [eval_policy(policy)]
+temp_val = []
+for t in range(int(args_max_timesteps)):
+    episode_timesteps += 1
+    counter += 1
+    # Select action randomly or according to policy
+    if t < args_start_timesteps:
+        action = np.random.uniform(-max_action, max_action, action_dim)
+    else:
+        if np.random.uniform(0, 1) < 0.1:
+            action = np.random.uniform(-max_action, max_action, action_dim)
+        else:
+            action = (
+                    policy.select_action(np.array(state))
+                    + np.random.normal(0, max_action * args_expl_noise, size=action_dim)
+            ).clip(-max_action, max_action)
+
+    # Perform action
+    next_state, reward, done, _ = env.step(action)
+    done_bool = float(done) if episode_timesteps < env._max_episode_steps else 0
+
+    replay_buffer.add(state, action, next_state, reward, done_bool)
+
+    state = next_state
+    episode_reward += reward
+
+    if t >= args_start_timesteps:
+        '''TD3'''
+        last_val = 999.
+        patient = 5
+        for i in range(1):
+            policy.train(replay_buffer, args_batch_size)
+
+    # Train agent after collecting sufficient data
+    if done:
+        print(
+            f"Total T: {t + 1} Episode Num: {episode_num + 1} Episode T: {episode_timesteps} Reward: {episode_reward:.3f}")
+        msk_list = []
+        state, done = env.reset(), False
+        episode_reward = 0
+        episode_timesteps = 0
+        episode_num += 1
+
+        # Evaluate episode
+    if (t + 1) % args_eval_freq == 0:
+        evaluations.append(eval_policy(policy))
+        print('recent Evaluation:', evaluations[-1])
+        np.save('results/evaluations_seed{}_alias{}_ENV{}'.format(seed,alias, ENV_NAME), evaluations)
